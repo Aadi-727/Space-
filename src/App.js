addce90e701f4fdaa47d25474e026a5c5abe026d
Zoyo <adithyanas1@gmail.com>
2021-12-11 16:31:12 +0530

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 // 🕉️🕉️🕉️🕉️🕉️🕉️🕉️
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
 	// Setup router with homepage
 	return (
-		<div className="App">
-			<Navbar />
-			<h1>Zoyo</h1>
-		</div>
+		<Router>
+			<div className="App">
+				<Navbar />
+			</div>
+		</Router>
 	);
 };
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import logo from "../../assets/shared/logo.svg";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import "./Navbar.css";
+
+// import "../../App.css";
+import Tab from "../Tab";
+const Navbar = () => {
+	return (
+		<nav className="navbar">
+			<img src={logo} alt="Logo" />
+			<ul className="navbar-links">
+				<Tab route="/" title="home" num="0" />
+				<Tab route="/destination" title="destination" num="01" />
+				<Tab route="/crew" title="crew" num="02" />
+				<Tab route="/technology" title="technology" num="03" />
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;

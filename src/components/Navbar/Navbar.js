74d7c9bfe49becf887bcdf65bcc6e2dc2d8c1616
Zoyo <adithyanas1@gmail.com>
2021-12-14 12:52:25 +0530

    Navbar bug

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,41 @@
-import logo from "../../assets/shared/logo.svg";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+// Image imports
+import { logo, ham, close } from "./imports.js";
+import { useState } from "react";
 import "./Navbar.css";
 
 // import "../../App.css";
 import Tab from "../Tab";
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <img src={logo} alt="Logo" />
-      <ul className="navbar-links">
-        <Tab route="/" title="home" num="00" />
-        <Tab route="/destination" title="destination" num="01" />
-        <Tab route="/crew" title="crew" num="02" />
-        <Tab route="/technology" title="technology" num="03" />
-      </ul>
-    </nav>
-  );
+	const [openNavbar, setOpenNavbar] = useState(false);
+
+	return (
+		<nav className="navbar">
+			<img className="logo" src={logo} alt="Logo" />
+			<img
+				className="ham"
+				style={{ display: openNavbar ? "none" : "inline-block" }}
+				onClick={() => setOpenNavbar(!openNavbar)}
+				src={ham}
+				alt="Hamburger menu"
+			/>
+
+			<ul
+				className="navbar-links"
+				style={{ display: openNavbar ? "flex" : "none" }}
+			>
+				<img
+					onClick={() => setOpenNavbar(!openNavbar)}
+					className="close-button"
+					src={close}
+					alt="Close Button"
+				/>
+				<Tab route="/" title="home" num="00" />
+				<Tab route="/destination" title="destination" num="01" />
+				<Tab route="/crew" title="crew" num="02" />
+				<Tab route="/technology" title="technology" num="03" />
+			</ul>
+		</nav>
+	);
 };
 
 export default Navbar;
---
+++ src/components/Navbar/imports.js
@@ -0,0 +1,5 @@
+import logo from "../../assets/shared/logo.svg";
+import ham from "../../assets/shared/icon-hamburger.svg";
+import close from "../../assets/shared/icon-close.svg";
+
+export { logo, ham, close };
